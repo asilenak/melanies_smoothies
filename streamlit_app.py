13082bbde9c64b0cf20b11a0cb500001a74d7520
asilenak <asil.enak@gmail.com>
2025-10-06 20:05:47 -0500

    Update streamlit_app.py
    adding call to SmoothieFroot API

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,8 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
         #st.success('Your Smoothie is ordered!', icon="✅")
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
